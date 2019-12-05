b46df17311023b4e4642da9f11bc62bb55fc4d6f
Jose Bob Jr. Santos <bobsantos@Joses-MacBook-Pro.local>
2019-12-05 23:58:08 +0200

    initial commit

---
+++ src/test/java/com/bobsantosjr/openapiv3/springdocsample/SpringdocSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobsantosjr.openapiv3.springdocsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
