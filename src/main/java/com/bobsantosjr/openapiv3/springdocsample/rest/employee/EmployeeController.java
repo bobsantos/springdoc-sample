a0171f8ac78837f6e8134c27588348a5a0b00e2d
bobsantos <bob.santosjr@gmail.com>
2020-01-20 22:30:15 +0200

    documentation

--- src/main/java/com/bobsantosjr/openapiv3/springdocsample/rest/employee/EmployeeController.java
+++ src/main/java/com/bobsantosjr/openapiv3/springdocsample/rest/employee/EmployeeController.java
@@ -2,6 +2,13 @@ package com.bobsantosjr.openapiv3.springdocsample.rest.employee;
 
 import com.bobsantosjr.openapiv3.springdocsample.core.employee.Employee;
 import com.bobsantosjr.openapiv3.springdocsample.core.employee.EmployeeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,26 +26,41 @@ public class EmployeeController {
         this.employeeRepository = employeeRepository;
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Creates an employees", responses = {
+        @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Employee.class)))
+    })
     public ResponseEntity<?> create(@RequestBody EmployeeCommand command) {
         employeeRepository.add(new Employee(null, command.getFirstName(), command.getLastName()));
         return ResponseEntity.ok(requireNonNull(employeeRepository.list().stream().reduce((first, second) -> second).orElse(null)));
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Retrieves all employees", responses = {
+        @ApiResponse(responseCode = "200", description = "List of employees", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = Employee.class)))),
+        @ApiResponse(responseCode = "404", description = "No employees found")
+    })
     public ResponseEntity<?> get() {
         List<Employee> employees = employeeRepository.list();
         return employees.size() > 0 ? ResponseEntity.ok(employeeRepository.list()) : ResponseEntity.notFound().build();
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<?> get(@PathVariable long id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Retrieves a single employee", responses = {
+        @ApiResponse(responseCode = "200", description = "Employee with given ID found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Employee.class))),
+        @ApiResponse(responseCode = "404", description = "Employee with given ID not found")
+    })
+    public ResponseEntity<?> get(@Parameter(description = "Unique employee ID") @PathVariable long id) {
         Optional<Employee> employee = employeeRepository.get(id);
-        return employee.isPresent() ? ResponseEntity.ok(employee.get()) : ResponseEntity.notFound().build();
+        return ResponseEntity.of(employee);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> delete(@PathVariable long id) {
+    @Operation(description = "Deletes an employee", responses = {
+        @ApiResponse(responseCode = "204", description = "Successfully deleted employee with given ID"),
+        @ApiResponse(responseCode = "404", description = "Employee with given ID not found")
+    })
+    public ResponseEntity<Void> delete(@Parameter(description = "Unique employee ID") @PathVariable long id) {
         return employeeRepository.delete(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 }
